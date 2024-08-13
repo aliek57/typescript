479327b936dc6afd76b9e9be596ac7e9b138253d
Keila <keilaprado@alunos.utfpr.edu.br>
2024-08-13 14:36:51 -0300

    first commit

---
+++ exemplo1/index.js
@@ -0,0 +1,6 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+;
+var result = add(3, 1);
+console.log(result);
---
+++ exemplo1/index.ts
@@ -0,0 +1,6 @@
+function add(n1: number, n2: number){
+    return n1+n2;
+};
+
+let result = add(3, 1);
+console.log(result);
