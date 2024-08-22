915e8ea32364834d91631cf0278d7bd597c9dfe5
Keila <keilaprado@alunos.utfpr.edu.br>
2024-08-22 15:57:06 -0300

    completed day 3

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,62 @@
-function add(n1: number, n2: number){
-    return n1+n2;
-};
+// function add(n1: number, n2: number){
+//     return n1+n2;
+// };
 
-let result = add(3, 1);
-console.log(result);
+// let result = add(3, 1);
+// console.log(result);
+
+// // tuple
+// let item: [string, number, boolean] = ['Apple', 3, true];
+
+// enum - mapeamento de informações
+// enum Role {
+//     admin =1,
+//     read =2,
+//     backup=3
+// }
+
+// const user = {
+//     firstName: 'keila',
+//     age: 25,
+//     role: Role.admin
+// }
+
+// console.log(user);
+
+//function void - valor indefinido
+// function result(price: number){
+//     console.log(price);
+// }
+
+// objeto
+// const pessoa = {
+//     firstName: 'keila',
+//     lastName: 'prado',
+//     age: 25
+// }
+
+// console.log(pessoa.lastName);
+
+// unkownn: util pra quando nao sei o tipo do valor e quero impor verificação antes de manipular dado
+// never: ponto do codigo que nao deve ser alcançado
+
+// Dia 3 - exercício 1
+// let pi = 3.14159;
+// let tau = pi * 2;
+
+// console.log(`${tau} is ${pi} times two.`)
+
+// // Dia 3 - exercício 2
+// let pie: string;
+// pie = 'apple';
+
+// console.log(`I like ${pie}-flavored pie.`);
+
+// Dia 3 - exercício 3
+// const sequence: number[] = Array.from(Array(10).keys());
+// const animals: string[] = ['macaco', 'jaburu', 'jabucreia'];
+// const sAndN: (number | string)[] = [1, 'dois', 3, 'quatro'];
+// const allMyArrays: (number | string)[][] = [sequence, animals, sAndN];
+
+// console.log(allMyArrays);
+
